979236f5b4c02db701dd2ab1f6270f0cdae4a9fb
Ivan Ermakov <ermakov.ia@phystech.edu>
2022-04-07 16:52:45 +0300

    ENH: minor changes and refactoring

--- stress.py
+++ stress.py
@@ -3,9 +3,9 @@ import subprocess
 import sys
 import random
 
+sampler_path = sys.argv[1]
 prog_path = sys.argv[2]
 ref_path = sys.argv[3]
-sampler_path = sys.argv[1]
 num = int(sys.argv[4])
 
 
@@ -14,15 +14,18 @@ for i in range(num):
     prog = subprocess.Popen([prog_path], stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
     ref = subprocess.Popen([ref_path], stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 
-    data = sampler.communicate()[0]
-    ref_data = ref.communicate(input = data)[0]
-    my_data = prog.communicate(input = data)[0]
+    data = sampler.communicate()[0].strip()
+    ref_data = ref.communicate(input = data)[0].strip()
+    my_data = prog.communicate(input = data)[0].strip()
 
     if (my_data != ref_data):
         print("Error!")
-        print("Data: ", data.decode())
-        print("Ref_answer: ", ref_data.decode())
-        print("My_answer: ", my_data.decode())
+        print("Data: ")
+        print(data.decode())
+        print("Ref_answer: ")
+        print(ref_data.decode())
+        print("My_answer: ")
+        print(my_data.decode())
         break
     else:
         print("OK")
